3b5027de9de877323e9855c48bd48b47a8837583
gabrielebarcellona <gabrerozabarcy5@gmail.com>
2023-04-12 14:09:34 +0200

    add structure java

---
+++ script.js
@@ -0,0 +1,21 @@
+// prezzo del biglietto per km(0,21)
+const priceKm = 0.21;
+console.log('priceKm' , priceKm);
+
+//sconto del 20% per i minorenni
+const discount20 = 20;
+console.log('discount20');
+
+//sconto del 40% per gli over 65
+const discount40 = 40;
+console.log('discount40');
+
+// età dell'utente 
+const age = prompt ("Inserisci la tua età");
+console.log('age' , age);
+
+//km che deve percorrere l'utente
+const kmUser = parseInt(prompt('Inserisci i km da percorrere' , 100));
+console.log('kmUser' , kmUser);
+
+
